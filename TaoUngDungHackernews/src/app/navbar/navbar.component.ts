7bb4326132fcf91264756c93374aeb3d81071d13
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-03-16 23:33:02 +0700

    Added folder UngDungHackernews

---
+++ TaoUngDungHackernews/src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  hello: string= "Hello everybody!"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
